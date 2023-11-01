45cecf8376e0bf18bb002b1d3ddb5d5f6b4ac14a
janocoop <jano.dreier@coop.ch>
2023-11-01 10:59:58 +0100

    eigene Version

--- arrays/src/main/java/org/example/Main.java
+++ arrays/src/main/java/org/example/Main.java
@@ -2,20 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[]numbers = new int[9];
-        numbers[0] = 11;
-        numbers[1] = 12;
-        numbers[2] = 13;
-        numbers[3] = 14;
-        numbers[3] = 15;
-        numbers[4] = 16;
-        numbers[5] = 17;
-        numbers[6] = 18;
-        numbers[7] = 19;
-        numbers[8] = 20;
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        int[] zahlen = new int[10];
+        zahlen[0] = 1;
+        zahlen[1] = 2;
+        zahlen[2] = 3;
+        zahlen[3] = 4;
+        zahlen[4] = 5;
+        zahlen[5] = 6;
+        zahlen[6] = 7;
+        zahlen[7] = 8;
+        zahlen[8] = 9;
+        zahlen[9] = 10;
+
+
+        for (int i = 0; i <= zahlen.length - 1; i++){
+            System.out.println(zahlen[i]);
         }
+
     }
 }
